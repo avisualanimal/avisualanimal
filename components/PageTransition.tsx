caeff3378daabcd732b901c80197a63c69c159bd
avisualanimal <info@avisualanimal.com>
2026-05-01 09:34:22 -0700

    Revert "Set page transition wrapper background to #051200"
    This reverts commit 0bf001bdcb2e31a23060fb5c82644b5de1c4ff43.

--- components/PageTransition.tsx
+++ components/PageTransition.tsx
@@ -14,7 +14,6 @@ export default function PageTransition({ children }: { children: React.ReactNode
         animate={{ opacity: 1 }}
         exit={{ opacity: 0 }}
         transition={{ duration: 0.4, ease: [0.22, 1, 0.36, 1] }}
-        style={{ backgroundColor: '#051200' }}
       >
         {children}
       </motion.div>
